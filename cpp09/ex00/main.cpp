1a418d287cdf1e7bb708f88c38faf52e3fc8db67
Pedro Manuel Pereira Coelho Bigote <pedperei@c1r13s3.42lisboa.com>
2023-10-11 15:19:02 +0100

    cpp 09 final fixes

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -7,9 +7,9 @@ int main(int argc, char **argv)
     std::string buff;
 
     
-    if (argc > 2)
+    if (argc != 2)
     {
-        std::cerr << "Error: too many arguments passed." << std::endl;
+        std::cerr << "Error: Wrong number of arguments passed." << std::endl;
         return (0);
     }
     BitcoinExchange *base = new BitcoinExchange();
@@ -17,12 +17,16 @@ int main(int argc, char **argv)
     if (!(ifile.is_open()))
     {
         std::cerr << "Error: could not open file." << std::endl;
+        delete base;
         return (0);
     }
 
     std::getline(ifile, buff);
     if (!base->check_header(buff, ',', 0))
+    {
+        delete base;
         return (0);
+    }
     while (std::getline(ifile, buff))
         base->extract_date_value(buff,',', 0);
     ifile.close();
@@ -31,11 +35,15 @@ int main(int argc, char **argv)
     if (!(ifile.is_open()))
     {
         std::cerr << "Error: could not open file." << std::endl;
+        delete base;
         return (0);
     }
     std::getline(ifile, buff);
     if (!base->check_header(buff, '|', 1))
+    {
+        delete base;
         return (0);
+    }
     while (std::getline(ifile, buff))
         base->extract_date_value(buff,'|', 1);
     ifile.close();
